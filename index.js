3b0983fabc5e2c76a16337d3605b2502aa098c94
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-02-17 12:03:52 +0600

    added user api

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+function jwtVerify(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ error: 'Invalid authorization header' });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ error: err.message });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 //connect database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rtntsud.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,11 +34,62 @@ async function run() {
     try {
         const users = client.db("arkMEDIA").collection("users");
         const posts = client.db("arkMEDIA").collection("posts");
+
+        //user
         
         app.post("/add-users", async (req, res) => {
             const user = req.body;
             const result = await users.insertOne(user);
             res.send(result);
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = {
+                email: email
+            };
+            const user = await users.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.TOKEN);
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: "" })
+        });
+
+        //verifyUser
+        const verifyUser = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await users.findOne(query);
+            if (!user) {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+            next();
+        };
+
+
+        //post
+        app.post('/add-posts',jwtVerify,verifyUser, async (req, res) => {
+            const id = req.params.id;
+            const post = req.body;
+            const result = await posts.insertOne(post);
+            const query = {};
+            const allpost = await posts.find(query).toArray();
+            res.send(allpost);
+
+        });
+
+        app.get('/get-posts',jwtVerify,verifyUser, async (req, res) => {
+            const query = {};
+            const allpost = await posts.find(query).toArray();
+            res.send(allpost);
+            
+        });
+        
+
+        app.patch('/like-post/:id',async (req, res) => {
+            const postId = req.params.id;
+
         });
 
     }
